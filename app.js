60b4aa273a25d5b23e929b4637fbb1910364a3f4
somil02 <somilgupta091@gmail.com>
2025-03-20 10:54:23 +0530

    [Somil Gupta] Add: UC11 implemented

--- app.js
+++ app.js
@@ -137,6 +137,12 @@ class AddressBook {
 
         return { countByCity, countByState };
     }
+
+    //sort person by name
+    sortContactsByName() {
+        this.contacts.sort((a, b) => a.firstName.localeCompare(b.firstName));
+        console.log('Sorted Contacts:', this.contacts.map(contact => contact.displayContact()));
+    }
 }
 
 const addressBook = new AddressBook();
@@ -170,3 +176,6 @@ console.log("Persons grouped by city and state:", addressBook.viewPersonsByCityO
 
 //getting count by city and state
 console.log("Count of contacts by city and state:", addressBook.getContactCountByCityOrState());
+
+// Sorting contacts by name
+addressBook.sortContactsByName();
